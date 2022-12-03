114b497620c09d84e7ee3dc22cf069cecf4d566b
Jake <104585445+jtridini@users.noreply.github.com>
2022-12-03 20:14:08 +0000

    Update 01122022.py
    Removing indents

--- 01122022.py
+++ 01122022.py
@@ -1,13 +1,10 @@
 def day1part1and2(file):
     line = file.readlines()
     line = [i.rstrip('\n') for i in line]
-
     i = 0
     #[print(len(i)) for i in line] # check length of emtpy string
-
     list = []
     val = 0
-
     while i < len(line):
         if len(line[i]) < 1:
             list.append(val)
@@ -15,10 +12,8 @@ def day1part1and2(file):
         else:
             val += int(line[i])
         i+=1
-
     top_total = max(list) # part 1
     top_three_total = sum(sorted(list, key=int, reverse=True)[0:3]) # part 2
-
     return top_total, top_three_total
 
 filepath = open(r"C:\Users\Jake\Documents\01122022-AoC-Calories-CSV.txt", 'r')
