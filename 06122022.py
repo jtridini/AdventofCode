c486be602cbb536d35750f4fcb766d52a3b725e9
Jake <jake.tridini@hotmail.com>
2022-12-06 23:46:48 +0000

    Removing redundant if statement

--- 06122022.py
+++ 06122022.py
@@ -3,13 +3,10 @@ def tuner(file,noOfCharacters):
         signal = f.readline()
     for i in range(len(signal)-noOfCharacters-1):
             marker = signal[i:i+noOfCharacters]
-            if len(set(marker)) < noOfCharacters:
-                pass
-            else:
-                startOfPacketMarker = i+noOfCharacters
+            if len(set(marker)) == noOfCharacters:
+                print(i + noOfCharacters)
                 break
-    return startOfPacketMarker
 
 filePath = r'C:\Users\Jake\Documents\06122022-DataSteam.txt'
-print(tuner(filePath,4))
-print(tuner(filePath,14))
+tuner(filePath,4)
+tuner(filePath,14)
